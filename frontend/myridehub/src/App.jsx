9c549f3d9b7ae414cd56431982727680c6c9bb4d
Danisse Cabana <cabanadanisse7@gmail.com>
2025-04-16 01:24:44 +0800

    fixed again failed secured

--- frontend/myridehub/src/App.jsx
+++ frontend/myridehub/src/App.jsx
@@ -4,7 +4,7 @@ import SignupPage from './Auth/SignupPage';
 import LoginPage from './Auth/LoginPage';
 import DashboardPage from './Dashboard/DashboardPage';
 import BookingPage from './Booking/BookingPage';
-import PrivateRoute from './components/PrivateRoute';
+//import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -12,22 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/booking"
-          element={
-            <PrivateRoute>
-              <BookingPage />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/booking" element={<BookingPage />} />
       </Routes>
     </Router>
   );
