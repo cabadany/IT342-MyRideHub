163ad9af410d5906913845d86493adcd3d4dcfcf
Hara Alexa Tumungha <haraalexa.tumungha@cit.edu>
2025-04-27 23:29:45 +0800

    payment backend

---
+++ backend/src/main/java/com/sia/myridehub/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.sia.myridehub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sia.myridehub.model.Payment;
+import com.sia.myridehub.service.PaymentService;
+
+@RestController
+@RequestMapping("/api/payments")
+@CrossOrigin(origins = "http://localhost:5173")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        Payment savedPayment = paymentService.savePayment(payment);
+        return ResponseEntity.ok(savedPayment);
+    }
+}
