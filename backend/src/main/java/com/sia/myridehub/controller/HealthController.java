5f6de58ee73540f70d970a6ebecc84a8ea19f9b0
Danisse Cabana <cabanadanisse7@gmail.com>
2025-04-30 23:03:53 +0800

    testing

---
+++ backend/src/main/java/com/sia/myridehub/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.sia.myridehub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "✅ RideHub backend is running!";
+    }
+}
