d3424547e766c617ac33b641be3d2d663c95877d
Danisse Cabana <cabanadanisse7@gmail.com>
2025-05-01 23:38:57 +0800

    changed some of the dto codes

--- backend/src/main/java/com/sia/myridehub/model/dto/LoginResponseDto.java
+++ backend/src/main/java/com/sia/myridehub/model/dto/LoginResponseDto.java
@@ -1,12 +1,11 @@
 package com.sia.myridehub.model.dto;
 
 public class LoginResponseDto {
-    
     private String token;
     private Long driverId;
     private String firstName;
     private String lastName;
-    
+
     public LoginResponseDto(String token, Long driverId, String firstName, String lastName) {
         this.token = token;
         this.driverId = driverId;
@@ -14,36 +13,19 @@ public class LoginResponseDto {
         this.lastName = lastName;
     }
 
-    // Getters and Setters
     public String getToken() {
         return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 
     public Long getDriverId() {
         return driverId;
     }
 
-    public void setDriverId(Long driverId) {
-        this.driverId = driverId;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
